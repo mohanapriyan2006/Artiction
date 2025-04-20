2fdebc2cd397a6e97abcd1505bde5523a8f0dc83
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-04-20 18:57:06 +0530

    commit7

--- frontend/artiction/vite.config.js
+++ frontend/artiction/vite.config.js
@@ -1,31 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { visualizer } from 'rollup-plugin-visualizer';
+
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
-    visualizer({
-      open: true,
-      filename: 'dist/stats.html' // Outputs analysis file
-    }),// Helps analyze bundles
   ],
-  build: {
-    outDir: '../dist',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  }
+
 })
 
 
