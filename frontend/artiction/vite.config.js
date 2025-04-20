ea0014a0bf1fdeb5dc6ab0f1afa4ed4a74646ca2
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-04-20 17:49:02 +0530

    commit

--- frontend/artiction/vite.config.js
+++ frontend/artiction/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/artiction/', 
+  base: '/', 
   build: {
     outDir: 'dist',
   },
