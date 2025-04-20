03aa1bba9463ec7a7a491b39eca401b6139ac922
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-04-20 16:55:26 +0530

    versel-file

--- frontend/artiction/vite.config.js
+++ frontend/artiction/vite.config.js
@@ -13,17 +13,3 @@ export default defineConfig({
     outDir: 'dist', // Default build folder (Vercel looks for this)
   },
 })
-
-/*
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Set this to './' if deploying to a subpath
-  build: {
-    outDir: 'dist', // Default build folder (Vercel looks for this)
-  },
-}); 
-*/
