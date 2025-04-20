8b74451d7e489141c9faccccd398871b6ae02dc1
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-04-20 16:53:00 +0530

    dist-created

--- frontend/artiction/src/components/Missing.jsx
+++ frontend/artiction/src/components/Missing.jsx
@@ -9,9 +9,9 @@ const Missing = () => {
 
   return (
     <div className="missing place-content-center place-items-center mt-10">
-      <h3 className='md:text-4xl sm:text-3xl text-2xl font-semibold'><b style={{ color: 'red' }}>404</b> Page not found !</h3>
-      <h5 className='md:text-4xl sm:text-3xl text-2xl font-semibold mt-8'> Go to <button onClick={() => { navigate('/'); setActive('home') }} className="btn-2 cursor-pointer">Home</button> </h5>
-      <img className='h-auto md:w-150 sm:w-120 w-100' src={error} alt='Not found image' />
+      <h3 className='sm:text-3xl text-2xl font-semibold'><b style={{ color: 'red' }}>404</b> Page not found !</h3>
+      <h5 className='sm:text-3xl text-2xl font-semibold mt-8'> Go to <button onClick={() => { navigate('/'); setActive('home') }} className="btn-2 cursor-pointer">Home</button> </h5>
+      <img className='h-auto sm:w-120 w-100' src={error} alt='Not found image' />
     </div>
   )
 }
--- frontend/artiction/vite.config.js
+++ frontend/artiction/vite.config.js
@@ -8,4 +8,22 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/', // Set this to './' if deploying to a subpath
+  build: {
+    outDir: 'dist', // Default build folder (Vercel looks for this)
+  },
 })
+
+/*
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Set this to './' if deploying to a subpath
+  build: {
+    outDir: 'dist', // Default build folder (Vercel looks for this)
+  },
+}); 
+*/
