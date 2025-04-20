3240a50fe13fbf37cf55de84f0043f28f1ed0373
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-04-20 17:27:25 +0530

    vercel-v3

--- frontend/artiction/vite.config.js
+++ frontend/artiction/vite.config.js
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/artiction/', 
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     react(),
     tailwindcss(),
